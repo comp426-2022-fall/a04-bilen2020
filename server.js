1b78648e8ce279ff3b7fe4215132a9f5ef224493
bilen2020 <64380790+bilen2020@users.noreply.github.com>
2022-12-08 16:32:11 -0500

    Fix

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ if(args.port){
 }
 
 const app = express();
-
-
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 
@@ -32,7 +31,6 @@ app.post('/app/roll/', (req, res) => {
     var sides = parseInt(req.body.sides);
     var dice = parseInt(req.body.dice);
     var rolls = parseInt(req.body.rolls);
-    console.log(req.body);
     res.send(roll(sides, dice, rolls));
    
 })
@@ -40,14 +38,14 @@ app.post('/app/roll/', (req, res) => {
 app.get('/app/roll/:sides/', (req, res) => {
     var sides = parseInt(req.params.sides);
     res.send(roll(sides,2,1));
-    
+    res.end;
 })
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     var sides = parseInt(req.params.sides);
     var dice = parseInt(req.params.dice);
     res.send(roll(sides, dice, 1));
-    
+    res.end
 })
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
@@ -55,13 +53,13 @@ app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     var dice = parseInt(req.params.dice);
     var rolls = parseInt(req.params.rolls);
     res.send(roll(sides,dice,rolls));
-    
+    res.end
 })
 
 app.get('*', (req, res) => {
     console.error('error')
     res.status(404).send('404 NOT FOUND');
-    
+    res.end
 })
 
 app.listen(port, () => {
