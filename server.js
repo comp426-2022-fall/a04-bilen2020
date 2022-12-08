bc1b5011fb94df1c9792463ed9fb608d4efa15aa
bilen2020 <64380790+bilen2020@users.noreply.github.com>
2022-12-08 16:35:06 -0500

    Fix

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.get('/app/', (req, res) => {
-    res.send('200 OK');
+    res.status(200).send('200 OK');
    
 })
 
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(6,2,1));
+    res.status(200).send(roll(6,2,1));
    
 })
 
@@ -31,20 +31,20 @@ app.post('/app/roll/', (req, res) => {
     var sides = parseInt(req.body.sides);
     var dice = parseInt(req.body.dice);
     var rolls = parseInt(req.body.rolls);
-    res.send(roll(sides, dice, rolls));
+    res.status(200).send(roll(sides, dice, rolls));
    
 })
 
 app.get('/app/roll/:sides/', (req, res) => {
     var sides = parseInt(req.params.sides);
-    res.send(roll(sides,2,1));
+    res.status(200).send(roll(sides,2,1));
     res.end;
 })
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
     var sides = parseInt(req.params.sides);
     var dice = parseInt(req.params.dice);
-    res.send(roll(sides, dice, 1));
+    res.status(200).send(roll(sides, dice, 1));
     res.end
 })
 
@@ -52,13 +52,13 @@ app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     var sides = parseInt(req.params.sides);
     var dice = parseInt(req.params.dice);
     var rolls = parseInt(req.params.rolls);
-    res.send(roll(sides,dice,rolls));
+    res.status(200).send(roll(sides,dice,rolls));
     res.end
 })
 
 app.get('*', (req, res) => {
     console.error('error')
-    res.status(404).send('404 NOT FOUND');
+    res.status(200).status(404).send('404 NOT FOUND');
     res.end
 })
 
